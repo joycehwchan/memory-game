282bf8683f07623c7664984e74d95cb3db4523f0
joycehwchan <joycehwchan@gmail.com>
2022-12-11 18:16:31 +0900

    update instructions

--- src/components/Instruction.js
+++ src/components/Instruction.js
@@ -1,4 +1,5 @@
 import "../styles/Instruction.css";
+import image from "../images/modal.png";
 import React, { useState } from "react";
 
 export const Instruction = () => {
@@ -8,15 +9,20 @@ export const Instruction = () => {
   const modal = (
     <div className="modal">
       <div className="modalBody">
-        <h3>名探偵コナン 記憶ゲーム</h3>
-        <p>
-          キャラクターをクリックしてポイントを獲得して、
-          <br />
-          同じキャラを2回クリックしたらやり直し！
-        </p>
-        <button type="button" className="startBtn" onClick={handleClick}>
-          スタート
-        </button>
+        <div>
+          <img src={image} alt="" />
+        </div>
+        <div>
+          <h3>名探偵コナン 記憶ゲーム</h3>
+          <p>
+            キャラクターをクリックしてポイントを獲得して、
+            <br />
+            同じキャラを2回クリックしたらやり直し！
+          </p>
+          <button type="button" className="startBtn" onClick={handleClick}>
+            スタート
+          </button>
+        </div>
       </div>
     </div>
   );
